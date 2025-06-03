7ba2829da9b2c52b4a8290760b2e77d3c05222e5
vldmv <jeongwoon1009@gmail.com>
2025-06-03 10:46:33 +0000

    완료

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import geopandas as gpd
+import folium
 
-st.title("🎈 My new app")
+st.title("🎈 My new afpp")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
