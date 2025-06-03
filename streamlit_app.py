966bec2b3bd70c2e96297474a013918d5a2b0289
vldmv <jeongwoon1009@gmail.com>
2025-06-03 11:05:14 +0000

    커밋

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
-#import geopandas as gpd
 import folium
 
-st.title("🎈 My new appㄹ")
+st.title("🎈 My new ㄹappㄹ")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
