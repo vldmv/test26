a726f7bf5acf6398b2b7a4e0d9e6b2cd8db286d5
vldmv <jeongwoon1009@gmail.com>
2025-06-04 14:44:15 +0000

    124

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 st.title("진주시 CCTV 현황")
 
-df = pd.read_csv("/content/jinju_cctv_20250513.csv", encoding='euc-kr')
+df = pd.read_csv("jinju_cctv_20250513.csv", encoding='euc-kr')
 
 st.dataframe(df, height=200)
 
